fa9b8f6cf46547f50c08bcb526351886615640d6
Adam Kearney <aklives@hotmail.com>
2019-04-11 22:46:15 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costume_stores do |c|
+      c.string :name
+      c.string :location
+      c.float :costume_inventory
+      c.float :num_of_employees
+      c.boolean :still_in_business
+      c.datetime :opening_time
+      c.datetime :closing_time
+
+    end
+  end
+
+end
